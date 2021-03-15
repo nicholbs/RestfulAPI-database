98b7c755c71da6a629307cf29e1df043884f210c
Odd Hald Bliksås <oddhb@stud.ntnu.no>
2021-03-15 18:29:26 +0100

    La til kommenter på controller

--- controller/controller.php
+++ controller/controller.php
@@ -4,6 +4,16 @@ class controller{
       //implementer logikk her
 
     }
+
+    /**
+     * Denne funksjonen motar en request fra api.php finner ut hvilket endpoint det skal til og sender informasjon videre
+     * til dette endpointet foreksempel customer. pr nå returnerer den kun vi eri kontroller, dette skal du se via din
+     * nettleser
+     * @param $dividedUri - Motar uri delt i dler
+     * @param $specificQuery - motar query parametre
+     * @param $requestType - om det er get, put osv
+     * @return string - returnerer en teststring pr nå
+     */
     public  function request($dividedUri,$specificQuery,$requestType){
         $test ="Vi er i kontroller";
         //legg på logikk her
