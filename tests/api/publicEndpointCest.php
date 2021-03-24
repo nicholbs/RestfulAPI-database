c0cdf46f035b38ce24930f94542045a09bb5f75e
Odd Hald Bliksås <oddhb@stud.ntnu.no>
2021-03-24 13:42:25 +0100

    oppdatert testen til a fungere på ny db

--- tests/api/publicEndpointCest.php
+++ tests/api/publicEndpointCest.php
@@ -9,25 +9,26 @@ class publicEndpointCest
     // tests
     public function tryToTest(ApiTester $I)
     {
-     $I ->sendGet('/public/skis?grip=dd');
+     $I ->sendGet('/public/skis?grip=IntelliWax');
      $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); // 200
     }
 
     public function gripfilter(ApiTester $I){
-        $I ->sendGet('/public/skis?grip=dd');
+        $I ->sendGet('/public/skis?grip=IntelliWax');
         $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); // 200
-        $I->seeResponseContainsJson(array('type_id' => 1,'grip'=> 'dd'), ); //Cheks taht we get return a skitype wit id 1
+        $I->seeResponseContainsJson(array('type_id' => 1,'grip'=> 'IntelliWax'), ); //Cheks taht we get return a skitype wit id 1
     }
+
     public function modelfilter(ApiTester $I){
-        $I ->sendGet('/public/skis?model=tester');
+        $I ->sendGet('/public/skis?model=Redline');
         $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); // 200
-       $I->seeResponseContainsJson(array('type_id' => 0,'model'=> 'tester'), ); //Cheks taht we get return a skitype with model tester
+       $I->seeResponseContainsJson(array('type_id' => 2,'model'=> 'Redline'), ); //Cheks taht we get return a skitype with model tester
     }
+
     public function modelandgripfilter(ApiTester $I){
-        $I ->sendGet('/public/skis?model=tester&grip=dd');
-        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); // 200
-        $I->seeResponseContainsJson(array('type_id' => 0,'model'=> 'tester'), );
-        $I->seeResponseContainsJson(array('type_id' => 1,'grip'=> 'dd'), ); //Cheks taht we get return a skitype wit id 1
+         $I ->sendGet('/public/skis?model=Redline&grip=IntelliWax');
+        $I->seeResponseContainsJson(array('type_id' => 1,'grip'=> 'IntelliWax'), );
+        $I->seeResponseContainsJson(array('type_id' => 2,'model'=> 'Redline'), );
     }
 
 
