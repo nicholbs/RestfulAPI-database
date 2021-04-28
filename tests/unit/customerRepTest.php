5d9bef82a87dac619dd7407b82ea98eaa2b98e53
Odd Hald Bliksås <oddhb@stud.ntnu.no>
2021-04-28 20:01:45 +0200

    Laget customerRep unit test for

--- tests/unit/customerRepTest.php
+++ tests/unit/customerRepTest.php
@@ -11,7 +11,7 @@ class customerRepTest extends \Codeception\Test\Unit
      * @var \UnitTester
      */
     protected $tester;
-    
+
     protected function _before()
     {
     }
@@ -25,73 +25,70 @@ class customerRepTest extends \Codeception\Test\Unit
     {
 
     }
-    public function testChangeOrderState(){   //Test som skal generere en feil
-        $token = "839d6517ec104e2c70ce1da1d86b1d89c5f547b666adcdd824456c9756c7e261";
-       $uri=array();
-        // $uri = [('customer-rep'), ('state')];
-        $uri[0]= "customer-rep";
-        $uri[1] = "state";
-        $requestMethod = 'PUT';
-        $specificQuery = array();
-        $requestBodyJson = array();
-        $requestBodyJson['orderNumber'] = 1;
-        $requestBodyJson['status'] = "open";
-        
-        $Successfull = false;
-        try {
-          $test = new customerRepEndpoint();
-          $test ->handleRequest($uri,$specificQuery,$requestMethod,$requestBodyJson,$token); 
-          $this ->tester->seeInDatabase('orders',['order_nr' =>'1','state' =>'open']);
-        } catch (APIException $event) {
-          $Successfull = true;
-        }
 
-        if ($Successfull == true) {
-          //testen er gått igjennom og er riktig
-        } else {
-          throw new APIException(httpErrorConst::badRequest, "Request generated an exception");
-        }
-       // $this->tester->seeInDatabase('order_history',['order_nr' =>'1','state' => 'open']);
+    public function testChangeOrderState()
+    {   //Test som skal generere en feil
 
-
-     /**
-        $resArr= array();
-        $emptyArr= array();
-        $resArr['orderNumber'] = 1;
-        $resArr['status'] = "open";
-
-        $testobj = new  customerRepModel();
-        $testobj ->changeOrderState($emptyArr, $resArr,$token);
-        $this->tester->seeInDatabase('orders', ['order_nr' => '1']);
-        **/
-      /**
-        $this->tester->seeInDatabase('orders',['order_nr' => 10,'state' => 'open']);
-        $this->tester->seeInDatabase('orders', array('order_nr' => 25));
-        $this ->tester->assert($this->tester->seeInDatabase('orders',['order_nr' => 10,'state' => 'open']));
-       * **/
+        /**
+         * $resArr= array();
+         * $emptyArr= array();
+         * $resArr['orderNumber'] = 1;
+         * $resArr['status'] = "open";
+         *
+         * $testobj = new  customerRepModel();
+         * $testobj ->changeOrderState($emptyArr, $resArr,$token);
+         * $this->tester->seeInDatabase('orders', ['order_nr' => '1']);
+         **/
+        /**
+         * $this->tester->seeInDatabase('orders',['order_nr' => 10,'state' => 'open']);
+         * $this->tester->seeInDatabase('orders', array('order_nr' => 25));
+         * $this ->tester->assert($this->tester->seeInDatabase('orders',['order_nr' => 10,'state' => 'open']));
+         * **/
     }
 
     /**
      * This function tests if we sucsessfully change the orderState
      */
-    public function changeOrderState(){
+    public function changeOrderState()
+    {
         $emptyArray = array();
         $requestBodyJson = array();
         $requestBodyJson['orderNumber'] = 1;
         $requestBodyJson['status'] = "open";
-        $token ="839d6517ec104e2c70ce1da1d86b1d89c5f547b666adcdd824456c9756c7e261";
+        $token = "839d6517ec104e2c70ce1da1d86b1d89c5f547b666adcdd824456c9756c7e261";
 
-        //$konvert= json_encode($requestBodyJson);
+        $test = new customerRepModel();
+        $test->changeOrderState($emptyArray, $requestBodyJson, $token);
+        $this->tester->seeInDatabase('orders', ['order_nr' => '1', 'state' => 'open']);
 
-        //try {
+
+    }
+
+    /**
+     * This function chek tath you get an error message if a user try to change with an nonexiting order.
+     */
+    public function testchangeOrderStateIlegal()
+    {
+        $emptyArray = array();
+        $requestBodyJson = array();
+        $requestBodyJson['orderNumber'] = 100;
+        $requestBodyJson['status'] = "open";
+        $token = "839d6517ec104e2c70ce1da1d86b1d89c5f547b666adcdd824456c9756c7e261";
+
+        $tes = false;
+        //because of busniess logick, we want this to faild and send a expetion error instead.
+        try {
             $test = new customerRepModel();
-           // $test->changeOrderState($emptyArray, $requestBodyJson, $token);
             $test->changeOrderState($emptyArray, $requestBodyJson, $token);
             //$this->tester->seeInDatabase('orders', ['order_nr' => '1', 'state' => 'open']);
-        //}
-        //catch (APIException $event){
-          //  console.log($event);
-       // }
 
+            //If the test failed with an expetin, we are happy.
+        } catch (BusinessException $l) {
+            $tes = true;
+
+        }
+    if($tes ==false){
+        throw new  BusinessException("33","sdsdsdds");
+    }
     }
 }
