87330e9401748f1908eea858792efcfa031be732
Oddbjørn Sivert Borge-Jensen <oddbjosb@stud.ntnu.no>
2021-04-29 14:13:45 +0200

    pirk

--- constants.php
+++ constants.php
@@ -28,3 +28,13 @@ class httpErrorConst{
     // 5xx Server Error
     const serverError = 500;
 }
+
+class HTTPConstants{
+    const OK = 200;
+
+    const BAD_REQUEST = 400;
+    const UNAUTHORIZED = 401;
+    const NOT_FOUND = 404;
+    
+    const SERVER_ERROR = 500;
+}
